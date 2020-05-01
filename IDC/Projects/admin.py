bcfa5ec0345e3199303e80563e6a089e0103e31a
X37OHSSVX <suyash.sc25@gmail.com>
2020-05-01 11:06:03 +0530

    Plz fix this shit

--- IDC/Projects/admin.py
+++ IDC/Projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Sponsor)
+admin.site.register(Students)
+admin.site.register(Faculties)
+admin.site.register(Image)
+
--- IDC/Projects/models.py
+++ IDC/Projects/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length = 100, null = False)
+    description = models.TextField()
+    department = models.CharField(max_length = 100)
+
+    project_lead = models.CharField(max_length = 100, blank = True)
+
+  
+
+
+class Faculties(models.Model):
+    name = models.CharField(max_length = 100, null = False)
+    department = models.CharField(max_length = 100)
+
+    projects = models.ManyToManyField(Project, blank = True, related_name="Faculties")
+
+
+
+class Students(models.Model):
+     name = models.CharField(max_length = 100, null = False)
+     roll_no = models.CharField(max_length = 9, null = False)
+
+     projects = models.ManyToManyField(Project, blank = True, related_name="Students")
+
+    
+class Image(models.Model):
+    image = models.ImageField("static")
+
+    projects = models.ManyToManyField(Project, blank = True, related_name="Image")
+
+    
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length = 100)
+    company = models.CharField(max_length = 100)
+
+    projects = models.ManyToManyField(Project, blank = True, related_name="Sponsor")
