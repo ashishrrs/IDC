a433b53b08e353ae1d0d2bfc63072a320e6fe34c
ashishrrs <ashish.18i061@gmail.com>
2020-05-04 18:12:13 +0530

    new commit by ashish

--- IDC/IDC/settings.py
+++ IDC/IDC/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'Projects',
     'Forum',
     'Clubs',
-    'django_instagram',
-    'sorl.thumbnail'
+    #'django_instagram',
+    #'sorl.thumbnail'
 ]
 
 MIDDLEWARE = [
--- IDC/IDC/urls.py
+++ IDC/IDC/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Projects.urls')),
-    path('', include('Clubs.urls'))
+    path('', include('Clubs.urls')),
+    path('',include('Forum.urls')),
 ]
