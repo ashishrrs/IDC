7b65097aec7e2ce395b5e54f4ca5d0a5dfdfc51a
hello-fri-end <shahanwaar.khalid8@gmail.com>
2020-05-05 14:56:26 +0530

    Added forum route & configured smtp to send mail to Ashish and me

--- IDC/IDC/settings.py
+++ IDC/IDC/settings.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS= True
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_HOST_USER= 'iiitdm.dic@gmail.com'
+EMAIL_HOST_PASSWORD= 'acakscsktk'
+
+
 
 # Application definition
 
