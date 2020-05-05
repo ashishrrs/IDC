261645992cef54904bae598e2f21c35323f31b7a
hello-fri-end <shahanwaar.khalid8@gmail.com>
2020-05-05 15:01:42 +0530

    Allah Akhbar

--- IDC/Forum/models.py
+++ IDC/Forum/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class queries(models.Model):
+    name= models.CharField(max_length=100)
+    email= models.EmailField()
+    message= models.TextField(max_length=500)
+    response_message= models.TextField(max_length=500, blank=True)
+    status= models.BooleanField(default= False)
+
+
+
