5fdd376d7842e2d5c6bf8c68d7a7f45964227b84
Shah Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2020-05-09 18:04:42 +0530

    Update urls.py

--- IDC/Forum/urls.py
+++ IDC/Forum/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('forms/', views.index)
+    path('forms/', views.index, name="forms"),
+    path("forms/<str:key>/", views.query_page,name="query")
 ]
