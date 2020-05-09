9f9c6e034296a4931ec9f75ae9eb87bee15414b3
Shah Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2020-05-09 18:04:13 +0530

    Update admin.py

--- IDC/Forum/admin.py
+++ IDC/Forum/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from. models import queries
+from. models import *
 # Register your models here.
-admin.site.register(queries)
+admin.site.register(query)
+admin.site.register(reply)
+
+
