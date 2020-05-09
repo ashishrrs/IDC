3f8694efe11ae96ae059695d13783c3b17ab9dc0
ashishrrs <ashish.18i061@gmail.com>
2020-05-09 19:30:57 +0530

    fixed carousel link

--- IDC/Projects/urls.py
+++ IDC/Projects/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("projects/", views.index,name ="index" ),
     path("projects/<str:key>/",views.project_display, name= "project_display"),
     path("project_add/",views.project_add, name = "project_display"),
-    path("carousel/",views.carousel,name = "carousel_display")
+    # path("carousel/",views.carousel,name = "carousel_display")
 ]
