82e297794a7af88cbe0b7ab9ee540b60bc86c0c9
Shah Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2020-05-09 18:05:10 +0530

    Update views.py

--- IDC/Forum/views.py
+++ IDC/Forum/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from .forms import Query
-from .models import queries
+from .models import *
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -32,5 +32,41 @@ from django.contrib.auth.decorators import login_required
 #             return render(request,"Forum/index.html",context )
 @login_required(login_url="/login/")
 def index(request):
-	return render(request,"Forum/index.html");
+	if request.method=='GET':
+		context= {
+			 "queries":query.objects.all()
+		 }
+		return render(request,"Forum/index.html",context)
+	else:
+		username= request.user.username
+		email = request.POST['email']
+		content= request.POST['new_discussion_text']
+		subject = request.POST['newdiscussiontitle']
+		message= "subject :" +subject +"\n" + "content:" +content
+		new_query = query(name= username, email= email, content= content, subject = subject)
+		new_query.save()
+		recipient_list= ['ced18i061@iiitdm.ac.in', 'ced18i048@iiitdm.ac.in', email]
+		send_mail(subject='New Query on DIC website',from_email= 'dic.iiitdmk@gmail.com',recipient_list = recipient_list, message=message)
+        
+
+		context= {
+			"queries": query.objects.all()
+		}
+		return render(request,"Forum/index.html", context)
+
+def query_page(request,key):
+	current_query= query.objects.get(pk= key)
+	responses= current_query.replies.all()
+	context= {
+		"current_query": current_query,
+		"responses": responses,
+	}
+	return render(request,"Forum/query.html", context)
+
+
+
+
+		
+
+
 
