6a3b175f9e10a5e349a16d3695387cacaa08114f
ShuKar99 <coe18b062@iiitdm.ac.in>
2020-05-10 19:28:18 +0530

    completing real data

--- IDC/Forum/models.py
+++ IDC/Forum/models.py
@@ -5,7 +5,7 @@ class query(models.Model):
 	name = models.CharField(max_length=20)
 	email = models.EmailField(blank=True)
 	content = models.TextField(max_length=500, blank= True)
-	subject= models.CharField(max_length=30)
+	subject= models.CharField(max_length=100)
 
 class reply(models.Model):
 	response= models.TextField(max_length=500)
