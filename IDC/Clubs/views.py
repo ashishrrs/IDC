796f9c8b9743d0c143fb27668d41a4bf6dbefec3
ShuKar99 <coe18b062@iiitdm.ac.in>
2020-05-10 06:47:03 +0530

    Homepage is done

--- IDC/Clubs/views.py
+++ IDC/Clubs/views.py
@@ -11,3 +11,13 @@ def index(request):
 	#create index route for displaying all clubs through carboard or whatever . . . 
 def insta_profile(request, instagram_profile_name):
 	return render(request,"Clubs/insta.html", {"instagram_profile_name": instagram_profile_name})
+
+
+
+def clubPage(request,key):
+	club = Club.objects.get(id = key)
+	print("oh hell\ncame here camhe here\n\n")
+	context = {
+		"club": club
+	}
+	return render(request,"Clubs/club.html",context)
